2c6293794164ee5c9524bff0cc699134356b210c
Anurag Sinha <anurag3487@gmail.com>
2022-07-15 16:45:49 +0530

    full solution commit

---
+++ FirstMVCSolution/FirstMVCProject/Controllers/SampleController.cs
@@ -0,0 +1,55 @@
+﻿using FirstMVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCProject.Controllers
+{
+    public class SampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Sample Controller - Index View");
+        }
+
+        public IActionResult Details()
+        {
+            return Content("Sample Controller - Details View");
+        }
+
+        public IActionResult RenderTable()
+        {
+            return View();
+        }
+
+        public IActionResult RenderTableUsingModels()
+        {
+            var persons = new List<PersonViewModel>
+            {
+                new PersonViewModel
+                {
+                    Id=1,
+                    Name="John",
+                    City="Chicago"
+                },
+                new PersonViewModel
+                {
+                    Id=2,
+                    Name="Mary",
+                    City="NY"
+                },
+                new PersonViewModel
+                {
+                    Id=3,
+                    Name="Dave",
+                    City="Jersey"
+                },
+                new PersonViewModel
+                {
+                    Id=4,
+                    Name="Mark",
+                    City="DC"
+                },
+            };
+            return View(persons);
+        }
+    }
+}
---
+++ FirstMVCSolution/FirstMVCProject/Models/PersonViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstMVCProject.Models
+{
+    public class PersonViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+}
