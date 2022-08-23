2dc09938717ac8eb48dd6694ef228b41e7bccbbd
Anurag Sinha <anurag3487@gmail.com>
2022-08-23 18:28:23 +0530

    Added custom validations using IValidatableObject Interface

---
+++ FirstMVCSolution/FirstMVCProject/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using FirstMVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FirstMVCProject.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new UserViewModel
+            {
+                Genders = PopulateGenders()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(UserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Genders = PopulateGenders();
+                return View(model);
+            }
+
+            //Once the model is validated properly, we can write code to save/update it to the DB.
+            //For simplicity purposes, just a content has been returned.
+            return Content("Validation checks have passed");
+        }
+
+        private List<SelectListItem> PopulateGenders()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem{Text="Male",Value="1"},
+                new SelectListItem{Text="Female",Value="2"},
+                new SelectListItem{Text="Others",Value="3"}
+            };
+        }
+    }
+}
---
+++ FirstMVCSolution/FirstMVCProject/Models/UserViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstMVCProject.Models
+{
+    public class UserViewModel : IValidatableObject
+    {
+        [Required(ErrorMessage = "Name can't be empty.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please select a gender.")]
+        public int? Gender { get; set; }
+
+        [Required(ErrorMessage = "City can't be empty.")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please enter required orders.")]
+        public int? Orders { get; set; }
+
+        public List<SelectListItem> Genders { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var customValidations = new List<ValidationResult>();
+
+            if (Orders.HasValue && (Orders.Value < 10 || Orders.Value > 100))
+            {
+                customValidations.Add(new ValidationResult("Orders are messed up.", new[] { "Orders" }));
+            }
+
+            if (!Name.Contains("an"))
+            {
+                customValidations.Add(new ValidationResult("Name is not proper.", new[] { "Name" }));
+            }
+
+            if (!Name.StartsWith("D") && !City.StartsWith("N"))
+            {
+                customValidations.Add(new ValidationResult("Name and City combination fails.", new[] { "Name", "City" }));
+            }
+            return customValidations;
+        }
+    }
+}
