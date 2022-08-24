50d5d7a4b094deff9f4da3979a260b8855ac0302
Anurag Sinha <anurag3487@gmail.com>
2022-08-24 10:29:31 +0530

    profile image custom tag helper

---
+++ FirstMVCSolution/FirstMVCProject/TagHelpers/ProfileImageTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace FirstMVCProject.TagHelpers
+{
+    [HtmlTargetElement(tag: "profile-image")]
+    public class ProfileImageTagHelper : TagHelper
+    {
+        public string ImageSrc { get; set; }
+        public int ImageWidth { get; set; }
+        public string ImageHeight { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "img";
+            output.TagMode = TagMode.SelfClosing;
+            output.Attributes.Add("src", ImageSrc);
+            output.Attributes.Add("height", ImageHeight);
+            output.Attributes.Add("width", ImageWidth);
+            output.Attributes.Add("style", "border-radius:50%");
+        }
+    }
+}
